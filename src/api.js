a014594f425fb0d0e25482fd1061f41c4d03082e
Matt Hobbs <matt@nooshu.com>
2020-07-07 23:36:20 +0100

    Netlify setup

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const serverless = require('serverless-http');
+const port = 3000;
+const path = require('path');
+const fs = require('fs');
+const router = express.Router();
+
+router.get('/with-flush', (req, res) => {
+    var startHtml = fs.readFileSync('./views/start.html');
+    var endHtml = fs.readFileSync('./views/end.html');
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(startHtml);
+    res.write(endHtml);
+    res.end();
+});
+
+router.get('/without-flush', (req, res) => {
+    var startHtml = fs.readFileSync('./views/start.html');
+    var endHtml = fs.readFileSync('./views/end.html');
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(startHtml + endHtml);
+    res.end();
+});
+
+app.use('/.netlify/functions/api', router);
+
+module.exports.handler = serverless(app);
