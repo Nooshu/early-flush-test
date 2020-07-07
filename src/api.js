60b574d8d5b4afbbff8c396ce03c1b43c99c1966
Matt Hobbs <matt@nooshu.com>
2020-07-07 23:47:29 +0100

    Update route

--- src/api.js
+++ src/api.js
@@ -16,6 +16,11 @@ router.get('/with-flush', (req, res) => {
     res.end();
 });
 
+router.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>This is a test!</h1>');
+})
+
 router.get('/without-flush', (req, res) => {
     var startHtml = fs.readFileSync('./views/start.html');
     var endHtml = fs.readFileSync('./views/end.html');
